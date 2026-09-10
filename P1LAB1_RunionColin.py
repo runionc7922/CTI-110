2aac1a44d1d1b9e700cf49b08dea5f9b270a7ca5
runionc7922 <runionc7922@student.faytechcc.edu>
2026-09-10 11:20:40 -0400

    Add files via upload
    This is the lab for Module 2.

---
+++ P1LAB1_RunionColin.py
@@ -0,0 +1,13 @@
+# Colin Runion
+# 9/10/2026
+# P1LAB1
+# input and print
+
+# Get first name
+first_name = input("Enter your first name: ")
+
+#Get last name
+last_name = input("Enter your last name: ")
+
+# Output welcome message to user
+print("Hello,", first_name,last_name +"!","Welcome to CTI-110!")
