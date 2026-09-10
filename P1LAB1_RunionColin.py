6ce03c73187d948f10ac75118d4b075e097d150b
runionc7922 <runionc7922@student.faytechcc.edu>
2026-09-10 10:59:35 -0400

    Delete P1LAB1_RunionColin.py
    Make room for the new version

--- P1LAB1_RunionColin.py
+++
@@ -1,9 +0,0 @@
-# Colin Runion
-# 9/10/2026
-# P1LAB1
-# input and print
-
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
-
-print("Hello,", first_name,last_name +"!","Welcome to CTI-110!")
