9bd16bad2196abeb5c23ccfa0c5e73c25fa9b642
runionc7922 <runionc7922@student.faytechcc.edu>
2026-09-10 11:57:07 -0400

    Add files via upload

---
+++ P1HW1_RunionColin.py
@@ -0,0 +1,38 @@
+# Colin Runion
+# 9/10/2026
+# Inputs, outputs, and mathematical calculations
+
+print("------- Calculating Exponents -------")
+print()
+print()
+
+# Get base value from user and convert to int
+base_value = int(input("Enter a base value: "))
+
+# Get exponent from user and convert to int
+exponent = int(input("Enter an exponent: "))
+
+# Calculate
+answer = base_value ** exponent
+
+# Display answer
+print(base_value, "raised to the power of", exponent, "is", answer)
+print()
+print()
+
+print("------- Addition and Subtraction -------")
+print()
+print()
+
+# Get three integers from user
+num1 = int(input("Enter starting number: "))
+num2 = int(input("Enter number to add: "))
+num3 = int(input("Enter number to subtract: "))
+
+print()
+
+# Display results
+print(num1, "+", num2, "-", num3, "=", num1 + num2 - num3)
+
+
+
